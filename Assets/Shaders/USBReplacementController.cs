f2ea70f1922b93af149da4328048a0f6476d2857
GBAC99 <g.bueno.acero@gmail.com>
2023-06-09 14:23:20 +0200

    Unity Import

---
+++ Assets/Shaders/USBReplacementController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+
+public class USBReplacementController : MonoBehaviour
+{
+    public Shader m_replacementShader; 
+
+    private void OnEnable() 
+    {
+        if (m_replacementShader!= null)
+        {
+            GetComponent<Camera>().SetReplacementShader(m_replacementShader, "RenderType");
+            Debug.Log("hola");
+        }    
+    }
+
+
+     private void OnDisable() 
+    {
+     
+            GetComponent<Camera>().ResetReplacementShader();
+    
+    }
+
+
+
+    
+}
